17a643e6f7f256521bd9787ab458394d73b173e2
github wubuchako <jfdev12@gmail.com>
2020-09-14 11:37:14 +0900

    add bookmarks

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContaner = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 //Show Modal, Focus on Input
 function showModal(){
     modal.classList.add('show-modal');
@@ -14,17 +16,70 @@ function showModal(){
 
 // Modal Event Listeners
 modalShow.addEventListener('click', showModal);
-modalClose.addEventListener('click', ()=> modal.classList.remove('show-modal'));
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal'): false));
+
+// Validate Form
+function validate(nameValue, urlValue){
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+    if(!nameValue || !urlValue){
+        alert('Please submit values for both fields.');
+        return false;
+    }
+    if(urlValue.match(regex)){
+        alert('Please provide a valid web sddress');
+        return false;
+    }
+
+    // Valid
+    return true;
+}
+
+// Fetch Bookmarks
+function fetchBookmarks(){
+    // Get bookmarks from localStorgae if available
+    if(localStorage.getItem('bookmarks')){
+        bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+    } else {
+    // Create bookamrks array in localStorage
+    bookmarks = [
+        {
+            name: 'Chiyomi Design',
+            url: 'https://',
+        },
+    ];
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    }
+    buildBookmarks();
+}
+
 
 //Handle Data from Form
 function storeBookmark(e){
     e.preventDefault();
-    console.log(e);
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    if(!urlValue.includes('http://', 'https://')){
+        urlValue = `https://${urlValue}`;
+    }
+    if(!validate(nameValue, urlVlaue)){
+        return false;
+    }
+    const bookmark = {
+        name: nameValue,
+        url: urlValue,
+    };
+    bookmarks.push(bookmark);
+
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+    fetchBookmarks();
+    bookmarkForm.reset();
+    websiteNameEl.focus();
 }
 
-
-// Ecent Listener
+// Event Listener
 bookmarkForm.addEventListener('submit', storeBookmark);
 
-
+//On load, fetch bookmarks
+fetchBookmarks();
